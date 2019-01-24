b94a7df23fb77be5a7c700eaeaf075795cf9b5a4
Warner Carpenter <warnercarpenter@yahoo.com>
2019-01-24 12:35:58 -0600

    can now post to api

--- scripts/journal.js
+++ scripts/journal.js
@@ -10,6 +10,7 @@ const makeJournalEntryComponent = (journalEntry) => {
             <h1>${journalEntry.concept}</h1>
             <p>${journalEntry.entry}</p>
             <p>${journalEntry.date}</p>
+            <p>Mood: ${journalEntry.mood}</p>
         </section>`
 }
 
@@ -20,4 +21,45 @@ const renderJournalEntries = (entries) => {
         entryComponent = makeJournalEntryComponent(entry)
         entryLog.innerHTML += entryComponent
     })
+}
+
+let journalDate = document.getElementById("journalDate")
+let conceptsCovered = document.getElementById("conceptsCovered")
+let journalEntry = document.getElementById("journalEntry")
+let mood = document.getElementById("mood")
+let entryButton = document.getElementById("entryButton")
+
+entryButton.addEventListener("click", function () {
+    addEntry()
+})
+
+const addEntry = () => {
+    let entry = {}
+    let journalDateValue = journalDate.value.replace(/-/g, "/")
+    let conceptsCoveredValue = conceptsCovered.value
+    let journalEntryValue = journalEntry.value
+    let moodValue = mood.value
+
+    journalDate.value = ""
+    conceptsCovered.value = ""
+    journalEntry.value = ""
+    mood.selectedIndex = 0
+
+    entry.date = journalDateValue
+    entry.concept = conceptsCoveredValue
+    entry.entry = journalEntryValue
+    entry.mood = moodValue
+
+    saveJournalEntry(entry)
+    entryLog.innerHTML += makeJournalEntryComponent(entry)
+}
+
+const saveJournalEntry = (journalEntry) => {
+    fetch("http://localhost:3000/entries", {
+     method: "POST",
+     headers: {
+       "Content-Type": "application/json"
+     },
+     body: JSON.stringify(journalEntry)
+   })
 }
